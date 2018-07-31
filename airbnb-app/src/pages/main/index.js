536c0d93679bf5812a2b5713e4b64dd1c7598d1d
Bruno Carvalho <bruno@brunocarvalho.me>
2018-07-31 15:03:27 -0300

    Add title and right button on main screen

--- airbnb-app/src/pages/main/index.js
+++ airbnb-app/src/pages/main/index.js
@@ -1,11 +1,35 @@
-import React from 'react';
-
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Button, TouchableOpacity, Text } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 // import styles from './styles';
 
-const Main = () => (
-  <View />
-);
+class Main extends Component {
+
+  static navigationOptions = ({ navigation, navigationOptions }) => {
+    const {params} = navigation.state;
+    
+    return {
+      title: 'Por perto',
+      headerRight: (
+        <MaterialIcons.Button onPress={() => params.handleOpen && params.handleOpen()} title="Shazam" name="more-vert" size={30} color="#fff" padding={0} margin={0} backgroundColor="transparent" />
+      ),
+    };
+  }
+
+  componentDidMount () {
+    this.props.navigation.setParams({handleOpen: () => this.openMoreOptions()});
+  }
+
+  openMoreOptions = () => {
+    alert('Open more options');
+  }
+
+  render() {
+    return(
+      <View />
+    )
+  }
+}
 
 export default Main;
--- airbnb-app/src/routes.js
+++ airbnb-app/src/routes.js
@@ -4,10 +4,23 @@ import SignIn from './pages/signIn';
 import SignUp from './pages/signUp';
 import Main from './pages/main';
 
-const Routes = createStackNavigator({
-  SignIn,
-  SignUp,
-  Main
-});
+const Routes = createStackNavigator(
+  {
+    SignIn,
+    SignUp,
+    Main
+  },
+  {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#ff5a60',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    }
+  }
+);
 
 export default Routes;
